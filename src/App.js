d8f096475bf4fd6b7243a7b851ed08d2d5a2838f
Francisco López <flopez_1384@hotmail.com>
2021-01-02 10:01:43 -0600

    [ADD] List component and mapping the data on this one

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
 import data from "./data";
+import List from "./List";
 
 function App() {
   const [people, setPeople] = useState(data);
-  return <h1>Hello my friend REACT with {people.length} more friends</h1>;
+  return (
+    <div>
+      <h1>Hello my friend REACT with {people.length} more firends </h1>
+      <List people={people} />
+    </div>
+  );
 }
 
 export default App;
