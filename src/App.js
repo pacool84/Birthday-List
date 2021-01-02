bd90d390821d5a95e31d2764b76c61da0504068a
Francisco López <flopez_1384@hotmail.com>
2021-01-02 09:26:17 -0600

    [ADD] The basic structure of teh project, cleaning the create-react-app files and chain data of the people that will be render on the list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import data from "./data";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [people, setPeople] = useState(data);
+  return <h1>Hello my friend REACT with {people.length} more friends</h1>;
 }
 
 export default App;
