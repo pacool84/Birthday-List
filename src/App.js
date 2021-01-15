63ab8b189560f7a2e1825794a7970e71fecf1947
Francisco López <flopez_1384@hotmail.com>
2021-01-15 10:24:04 -0600

    [ADD] The logic to refresh all the data if the list was cleared

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ import List from "./List";
 
 function App() {
   const [people, setPeople] = useState(data);
+  if (people.length === 0) {
+    return (
+      <div className='app'>
+        <h1>No hay mas personas en mi Listado</h1>
+        <button onClick={() => window.location.reload()}>
+          Recargar Mi Listado
+        </button>
+      </div>
+    );
+  }
   return (
     <div className='app'>
       <h1>{people.length} personas en mi listado </h1>
